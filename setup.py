af89fff9650c292429e4aa2e65528c45109684ec
frankid87 <frankid87@hotmail.it>
2020-08-24 10:52:24 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+config = {
+    'name': 'testpipegg',
+    'description': '',
+    'author': 'Helixa.ai',
+    'url': 'https://github.com/H-project-world/h2',
+    'packages': find_packages(),
+    'version':'1.0'
+}
+
+setup(**config)
